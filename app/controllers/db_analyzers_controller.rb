6193f448c5fa86d7412f9523fa8403eb83cae084
Abdullah Medhat <abdullahmedhat@192.168.1.15>
2025-02-08 14:56:01 +0200

    Create table insigh

--- app/controllers/db_analyzers_controller.rb
+++ app/controllers/db_analyzers_controller.rb
@@ -1,7 +1,5 @@
 class DbAnalyzersController < ApplicationController
-  DEFAULT_RAILS_TABLES = %w[ar_internal_metadata schema_migrations]
-
   def dashboard
-    @table_names = ActiveRecord::Base.connection.tables - DEFAULT_RAILS_TABLES
+    @table_names = ActiveRecord::Base.connection.tables - TableInsight::DEFAULT_RAILS_TABLES
   end
 end
--- app/models/table_insight.rb
+++ app/models/table_insight.rb
@@ -1,2 +1,3 @@
 class TableInsight < ApplicationRecord
+  DEFAULT_RAILS_TABLES = %w[ar_internal_metadata schema_migrations table_insights]
 end
